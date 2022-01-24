7aeee270ac066a1cdce735f3ec417fd0eeed245c
Evan <zx086401@gmail.com>
2022-01-24 11:36:57 +0800

    fix title

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to React</h1>
+          <h1>Fib calculator</h1>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>OtherPage</Link>
         </header>
